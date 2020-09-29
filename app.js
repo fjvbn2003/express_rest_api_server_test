f0a89f04efd1a1efe5c87e2826fc78b5036903b3
Nev3rd1Ekil1eR <ljygenius@gmail.com>
2020-09-29 23:10:56 +0900

    Update app.js
    제 PC 환경세어 포트 충돌 발생에 따라 변경 요청합니다.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 let meas_router = require('./routers/measure')
-const port = 443
+const port = 2020
 
 app.get('/', (req, res) => {
   res.send('정밀측정장비를 원격으로 다루기 위한 node.js express 기반 REST API 서버 입니다.')
