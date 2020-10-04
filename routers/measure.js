8a54dc6a3c2b860da8d91f4d3953ad1c5c7d9a22
ginami0129g <ginami0129n@naver.com>
2020-10-04 23:29:46 +0900

    Edit measure.js
    문법오류 수정했습니다.

--- routers/measure.js
+++ routers/measure.js
@@ -1,92 +1,111 @@
-var express = require('express');
-var GPIB = require('linux-gpib');
+var express = require("express");
+var GPIB = require("linux-gpib");
 
-const gpib = GPIB(0)
-let dmm = gpib.connect({ pad :13 })
+const gpib = GPIB(0);
+let dmm = gpib.connect({ pad: 13 });
 
 const router = express.Router(); // 새로 생겼어요!
 
-
 let voltages = [
-    {volt: 100},
-    {volt: 200},
-    {volt: 300},
-    {volt: 400},
-    {volt: 500},
-    {volt: 600},
-    
-]
+  { volt: 100 },
+  { volt: 200 },
+  { volt: 300 },
+  { volt: 400 },
+  { volt: 500 },
+  { volt: 600 },
+];
 
 let currents = [
-    {curr: 20},
-    {curr: 30},
-    {curr: 40},
-    {curr: 50},
-    {curr: 60},
-    {curr: 70},
-]
+  { curr: 20 },
+  { curr: 30 },
+  { curr: 40 },
+  { curr: 50 },
+  { curr: 60 },
+  { curr: 70 },
+];
 
-router.get('/volt/ac', (req, res)=>{
-       dmm.query('MEAS:VOLT:AC?').then((response) => {
-        let data = [];
-        return data ;      
-    }).then(data) => {
-    res.json(data)
-    dmm.disconnect();
-    }
-})
-router.get('/volt/dc', (req, res)=>{
-    dmm.query('MEAS:VOLT:DC?').then((response) => {
-        let data = [];
-        return data ;      
-    }).then(data) => {
-    res.json(data)
-    dmm.disconnect();
-    }
-})
-router.get('/curr/ac', (req, res)=>{
-      dmm.query('MEAS:CURR:AC?').then((response) => {
-        let data = [];
-        return data ;      
-    }).then(data) => {
-    res.json(data)
-    dmm.disconnect();
-    }
-})
-router.get('/curr/dc', (req, res)=>{
-        dmm.query('MEAS:CURR:DC?').then((response) => {
-        let data = [];
-        return data ;      
-    }).then(data) => {
-    res.json(data)
-    dmm.disconnect();
-    }
-})
-router.get('/res/2w', (req, res)=>{
-        dmm.query('MEAS:RES?').then((response) => {
-        let data = [];
-        return data ;      
-    }).then(data) => {
-    res.json(data)
-    dmm.disconnect();
-    }
-})
-router.get('/res/4w', (req, res)=>{
-        dmm.query('MEAS:FRES?').then((response) => {
-        let data = [];
-        return data ;      
-    }).then(data) => {
-    res.json(data)
-    dmm.disconnect();
-    }
-})
-router.get('/freq', (req, res)=>{
-        dmm.query('MEAS:FREQ?').then((response) => {
-        let data = [];
-        return data ;      
-    }).then(data) => {
-    res.json(data)
-    dmm.disconnect();
-    }
-})
-module.exports = router 
+router.get("/volt/ac", (req, res) => {
+  dmm
+    .query("MEAS:VOLT:AC?")
+    .then((response) => {
+      let data = [];
+      return data;
+    })
+    .then((data) => {
+      res.json(data);
+      dmm.disconnect();
+    });
+});
+router.get("/volt/dc", (req, res) => {
+  dmm
+    .query("MEAS:VOLT:DC?")
+    .then((response) => {
+      let data = [];
+      return data;
+    })
+    .then((data) => {
+      res.json(data);
+      dmm.disconnect();
+    });
+});
+router.get("/curr/ac", (req, res) => {
+  dmm
+    .query("MEAS:CURR:AC?")
+    .then((response) => {
+      let data = [];
+      return data;
+    })
+    .then((data) => {
+      res.json(data);
+      dmm.disconnect();
+    });
+});
+router.get("/curr/dc", (req, res) => {
+  dmm
+    .query("MEAS:CURR:DC?")
+    .then((response) => {
+      let data = [];
+      return data;
+    })
+    .then((data) => {
+      res.json(data);
+      dmm.disconnect();
+    });
+});
+router.get("/res/2w", (req, res) => {
+  dmm
+    .query("MEAS:RES?")
+    .then((response) => {
+      let data = [];
+      return data;
+    })
+    .then((data) => {
+      res.json(data);
+      dmm.disconnect();
+    });
+});
+router.get("/res/4w", (req, res) => {
+  dmm
+    .query("MEAS:FRES?")
+    .then((response) => {
+      let data = [];
+      return data;
+    })
+    .then((data) => {
+      res.json(data);
+      dmm.disconnect();
+    });
+});
+router.get("/freq", (req, res) => {
+  dmm
+    .query("MEAS:FREQ?")
+    .then((response) => {
+      let data = [];
+      return data;
+    })
+    .then((data) => {
+      res.json(data);
+      dmm.disconnect();
+    });
+});
+module.exports = router;
