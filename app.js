cf2b8cdae3b3316019e569f896a0b23631d2fc2a
ginami0129g <ginami0129n@naver.com>
2020-10-06 21:15:35 +0900

    Create .prettierrc
    세팅은 다음과 같습니다.
    문자열을 사용 할 때에는 ' 를 사용합니다.
    코드는 ; 로 끝나야합니다.
    탭 대신에 스페이스를 사용합니다.
    들여쓰기 크기는 2칸입니다.
    객체나 배열을 작성 할 때, 원소 혹은 key-value 의 맨 뒤에있는 것에도 쉼표를 붙입니다.
    한 줄이 80칸이 넘지 않도록 합니다

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-const express = require('express')
-const app = express()
-let meas_router = require('./routers/measure')
-const port = 2020
+const express = require("express");
+const app = express();
+let meas_router = require("./routers/measure");
+const port = 2020;
 
-app.get('/', (req, res) => {
-  res.send('정밀측정장비를 원격으로 다루기 위한 node.js express 기반 REST API 서버 입니다.')
-})
-app.use('/meas',meas_router)
+app.get("/", (req, res) => {
+  res.send(
+    "정밀측정장비를 원격으로 다루기 위한 node.js express 기반 REST API 서버 입니다."
+  );
+});
+app.use("/meas", meas_router);
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
